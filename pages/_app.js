0a633cface5f28122086dfd4671b0d6650e4656b
IvanSmiths <info@ivansmiths.com>
2021-05-28 08:26:43 +0200

    ADD Favicon

--- pages/_app.js
+++ pages/_app.js
@@ -18,6 +18,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta content="IE=edge" http-equiv="X-UA-Compatible" />
         <meta content="#fee648" name="theme-color" />
