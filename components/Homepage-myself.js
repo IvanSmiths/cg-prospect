86cb42278f2906ff97a379a6af7f12db2f15814d
IvanSmiths <info@ivansmiths.com>
2021-06-03 19:54:27 +0200

    ADD Pt translation

--- components/Homepage-myself.js
+++ components/Homepage-myself.js
@@ -17,6 +17,18 @@ function Myself() {
       <div className="myself-text">
         <h2 className="big-font">{t('home:myself-title')}</h2>
         <p className="small-font">{t('home:myself-desc')}</p>
+        <h3 className="small-font">
+          {t('home:myself-translate')}{' '}
+          <a
+            target="_blank"
+            rel="noopener"
+            href={t('home:myself-translate-link')}
+          >
+            <span className="highlight underline">
+              {t('home:myself-translate-by')}
+            </span>{' '}
+          </a>
+        </h3>
       </div>
     </section>
   );
