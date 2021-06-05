d97dfea8deed2238509f8d3b31361ddbba319e22
IvanSmiths <info@ivansmiths.com>
2021-06-05 18:46:33 +0200

    ADD Copy 3d

--- components/Homepage-3dmodel.js
+++ components/Homepage-3dmodel.js
@@ -4,23 +4,25 @@ const Homepage3dModel = () => {
   return (
     <section>
       <div className="hp-3d-model-cnt">
-        <h3 className="big-font">3D Models of everything</h3>
+        <h3 className="big-font">3D Models like they were real</h3>
         <p className="small-font highlight">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore
-          consequuntur eius debitis quidem sint. Quisquam qui rem omnis
-          accusantium asperiores?
+          Photogrammetry based 3d models, get the best for your renders. CG
+          Prospect is the answer to: Where i can find free to download 3D
+          models?
         </p>
         <Link href="/freee-3d-models">
-          <a className="btn-line">Browse 3d models</a>
+          <a className="btn-line">Go to realism</a>
         </Link>
       </div>
       <div className="flex-50 model-3d-example-cnt">
         <div>
-          <h4 className="medium-font">Extreme realism</h4>
+          <h4 className="medium-font">Photorealism</h4>
           <p className="small-font">
-            3d models free to download? Got you covered! You can find 3d models
-            of food, rocks and various objects in both low poly and high poly,
-            all free to use in your commercial projects.
+            Using the photogrammetry method, the 3d models from CG Prospect are
+            made in a photographic studio environmet, using professional lights
+            and equipment, granting production ready 3d assets. You can find 3d
+            models of food, rocks, furnitures and more, in both low poly and
+            high poly, so you can put them in your renders or games!
           </p>
           <Link href="/freee-3d-models">
             <a className="btn-line">Browse 3d models</a>
@@ -47,7 +49,7 @@ const Homepage3dModel = () => {
           />
         </div>
         <div>
-          <h4 className="medium-font">Extreme realism</h4>
+          <h4 className="medium-font">Optimized mesh</h4>
           <p className="small-font">
             3d models free to download? Got you covered! You can find 3d models
             of food, rocks and various objects in both low poly and high poly,
