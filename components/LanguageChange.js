6421aa2206e8cb46b920b0ccd84049b80e772e9e
IvanSmiths <info@ivansmiths.com>
2021-06-06 07:59:00 +0200

    MOD Navbar

--- components/LanguageChange.js
+++ components/LanguageChange.js
@@ -7,13 +7,23 @@ const LanguageChange = () => {
   let router = useRouter();
   return (
     <div className="lang-cnt-2">
-      <img
-        onMouseEnter={() => setIsOpened(!isOpened)}
-        src="/translate.svg"
-        height="39px"
-        width="39px"
-        alt="change languagge"
-      />
+      <div className="arrow-cnt">
+        <img
+          className="arrow-navbar2"
+          loading="lazy"
+          src="/down-arrow-white.svg"
+          height="13px"
+          width="13px"
+          alt="arrow down"
+        />
+        <img
+          onMouseEnter={() => setIsOpened(!isOpened)}
+          src="/translate.svg"
+          height="36px"
+          width="36px"
+          alt="change language"
+        />
+      </div>
       {isOpened && (
         <div className="dropdown-menu2" onMouseLeave={() => setIsOpened(false)}>
           <ol className="lang-cnt">
