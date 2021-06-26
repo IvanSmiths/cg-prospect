afc214ca4a163dac0842f14e6872ed0ecdaab432
IvanSmiths <info@ivansmiths.com>
2021-06-26 16:43:14 +0200

    !!!!!!!!!! REMOVE OLD I18N !!!!!!!!!

--- next.config.js
+++ next.config.js
@@ -2,47 +2,5 @@ const nextTranslate = require('next-translate');
 
 module.exports = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['en', 'it'],
-    defaultLocale: 'en',
-  },
   ...nextTranslate(),
-  async rewrites() {
-    return [
-      {
-        source: '/with-locale', // automatically handles all locales
-        destination: '/another', // automatically passes the locale on
-      },
-      {
-        // does not handle locales automatically since locale: false is set
-        source: '/nl/with-locale-manual',
-        destination: '/nl/another',
-        locale: false,
-      },
-      {
-        // this matches '/' since `en` is the defaultLocale
-        source: '/en',
-        destination: '/en/free-textures',
-        locale: false,
-      },
-      {
-        // this matches '/' since `en` is the defaultLocale
-        source: '/en',
-        destination: '/en/free-3d-models',
-        locale: false,
-      },
-      {
-        // this gets converted to /(en|fr|de)/(.*) so will not match the top-level
-        // `/` or `/fr` routes like /:path* would
-        source: '/(en)',
-        destination: '/free-textures',
-      },
-      {
-        // this gets converted to /(en|fr|de)/(.*) so will not match the top-level
-        // `/` or `/fr` routes like /:path* would
-        source: '/(en)',
-        destination: '/free-3d-models',
-      },
-    ];
-  },
 };
