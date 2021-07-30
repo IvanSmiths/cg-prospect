1f9d4fffc7b284729b407b95ec777fa8c71a05f4
IvanSmiths <info@ivansmiths.com>
2021-07-30 07:45:17 +0200

    ADD SLUG

--- pages/authors/index.js
+++ pages/authors/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import prisma from '../../lib/prisma';
 
-function Authors() {
-  return <div>aa</div>;
+function Authors({ authors }) {
+  console.log(authors);
+  return (
+    <main>
+      {authors.map((author) => (
+        <div key={author.id}>
+          <h2 className="big-font">{author.name}</h2>
+        </div>
+      ))}
+    </main>
+  );
 }
 
 export default Authors;
+
+export async function getStaticProps() {
+  const authors = await prisma.user.findMany({
+    orderBy: {
+      id: 'desc',
+    },
+    include: {
+      textures: true,
+      _count: {
+        select: {
+          textures: true,
+        },
+      },
+    },
+  });
+  return {
+    props: {
+      authors,
+    },
+  };
+}
