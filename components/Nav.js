252dabb7c89583ff426780ad1132e17a20f259e5
IvanSmiths <info@ivansmiths.com>
2021-08-15 14:36:52 +0200

    ADD Components

--- components/Nav.js
+++ components/Nav.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Dropdown from './Dropdown';
 import Dropdown2 from './Dropdown2';
@@ -9,6 +9,26 @@ import useTranslation from 'next-translate/useTranslation';
 import LanguageChange from './LanguageChange';
 
 function Navbar() {
+  const [isDesktop, setDesktop] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 1100) {
+      setDesktop(true);
+    } else {
+      setDesktop(false);
+    }
+
+    const updateMedia = () => {
+      if (window.innerWidth > 1100) {
+        setDesktop(true);
+      } else {
+        setDesktop(false);
+      }
+    };
+    window.addEventListener('resize', updateMedia);
+    return () => window.removeEventListener('resize', updateMedia);
+  }, []);
+
   let { t } = useTranslation();
   const [click, setClick] = useState(false);
   const [dropdown, setDropdown] = useState(false);
@@ -22,7 +42,7 @@ function Navbar() {
   const closeMobileMenu = () => setClick(false);
 
   const onMouseEnter = () => {
-    if (window.innerWidth < 960) {
+    if (window.innerWidth < 1100) {
       setDropdown(false);
     } else {
       setDropdown(true);
@@ -30,7 +50,7 @@ function Navbar() {
   };
 
   const onMouseLeave = () => {
-    if (window.innerWidth < 960) {
+    if (window.innerWidth < 1100) {
       setDropdown(false);
     } else {
       setDropdown(false);
@@ -38,7 +58,7 @@ function Navbar() {
   };
 
   const onMouseEnter2 = () => {
-    if (window.innerWidth < 960) {
+    if (window.innerWidth < 1100) {
       setDropdown2(false);
     } else {
       setDropdown2(true);
@@ -46,7 +66,7 @@ function Navbar() {
   };
 
   const onMouseLeave2 = () => {
-    if (window.innerWidth < 960) {
+    if (window.innerWidth < 1100) {
       setDropdown2(false);
     } else {
       setDropdown2(false);
@@ -54,7 +74,7 @@ function Navbar() {
   };
 
   const onMouseEnter3 = () => {
-    if (window.innerWidth < 960) {
+    if (window.innerWidth < 1100) {
       setDropdown3(false);
     } else {
       setDropdown3(true);
@@ -62,14 +82,14 @@ function Navbar() {
   };
 
   const onMouseLeave3 = () => {
-    if (window.innerWidth < 960) {
+    if (window.innerWidth < 1100) {
       setDropdown3(false);
     } else {
       setDropdown3(false);
     }
   };
   const onMouseEnter4 = () => {
-    if (window.innerWidth < 960) {
+    if (window.innerWidth < 1100) {
       setDropdown4(false);
     } else {
       setDropdown4(true);
@@ -77,14 +97,14 @@ function Navbar() {
   };
 
   const onMouseLeave4 = () => {
-    if (window.innerWidth < 960) {
+    if (window.innerWidth < 1100) {
       setDropdown4(false);
     } else {
       setDropdown4(false);
     }
   };
   const onMouseEnter5 = () => {
-    if (window.innerWidth < 960) {
+    if (window.innerWidth < 1100) {
       setDropdown5(false);
     } else {
       setDropdown5(true);
@@ -92,7 +112,7 @@ function Navbar() {
   };
 
   const onMouseLeave5 = () => {
-    if (window.innerWidth < 960) {
+    if (window.innerWidth < 1100) {
       setDropdown5(false);
     } else {
       setDropdown5(false);
@@ -100,160 +120,319 @@ function Navbar() {
   };
 
   return (
-    <nav className="navbar">
-      <div className="navbar-logo-cnt">
-        <Link href="/">
-          <a>
-            <img
-              src="/logo-navbar.svg"
-              height="30px"
-              width="160px"
-              alt="main logo"
-            />
-          </a>
-        </Link>
-      </div>
-      <LanguageChange />
-      <div>
-        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-          <li
-            className="nav-item small-font"
-            onMouseEnter={onMouseEnter}
-            onMouseLeave={onMouseLeave}
-          >
-            <img
-              className="arrow-navbar"
-              loading="lazy"
-              src="/down-arrow-white.svg"
-              height="13px"
-              width="13px"
-              alt="arrow down"
-            />
-            <Link
-              href="/free-textures"
-              className="nav-links"
-              onClick={closeMobileMenu}
-            >
-              <a>{t('common:nav-assets')}</a>
+    <div>
+      {isDesktop ? (
+        <nav className="navbar">
+          <div className="navbar-logo-cnt">
+            <Link href="/">
+              <a>
+                <img
+                  src="/logo-navbar.svg"
+                  height="30px"
+                  width="160px"
+                  alt="main logo"
+                />
+              </a>
             </Link>
-            {dropdown && <Dropdown />}
-          </li>
-          <li
-            onMouseEnter={onMouseEnter3}
-            onMouseLeave={onMouseLeave3}
-            className="nav-item small-font"
-          >
-            <img
-              className="arrow-navbar"
-              loading="lazy"
-              src="/down-arrow-white.svg"
-              height="13px"
-              width="13px"
-              alt="arrow down"
-            />
-            <Link
-              href="/categories"
-              className="nav-links"
-              onClick={closeMobileMenu}
-            >
-              <a>{t('common:nav-categories')}</a>
+          </div>
+          <LanguageChange />
+          <div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+              <li
+                className="nav-item small-font"
+                onMouseEnter={onMouseEnter}
+                onMouseLeave={onMouseLeave}
+              >
+                <img
+                  className="arrow-navbar"
+                  loading="lazy"
+                  src="/down-arrow-white.svg"
+                  height="13px"
+                  width="13px"
+                  alt="arrow down"
+                />
+                <Link
+                  href="/free-textures"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  <a>{t('common:nav-assets')}</a>
+                </Link>
+                {dropdown && <Dropdown />}
+              </li>
+              <li
+                onMouseEnter={onMouseEnter3}
+                onMouseLeave={onMouseLeave3}
+                className="nav-item small-font"
+              >
+                <img
+                  className="arrow-navbar"
+                  loading="lazy"
+                  src="/down-arrow-white.svg"
+                  height="13px"
+                  width="13px"
+                  alt="arrow down"
+                />
+                <Link
+                  href="/categories"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  <a>{t('common:nav-categories')}</a>
+                </Link>
+                {dropdown3 && <Dropdown3 />}
+              </li>
+              <li
+                onMouseEnter={onMouseEnter4}
+                onMouseLeave={onMouseLeave4}
+                className="nav-item small-font"
+              >
+                <img
+                  className="arrow-navbar"
+                  loading="lazy"
+                  src="/down-arrow-white.svg"
+                  height="13px"
+                  width="13px"
+                  alt="arrow down"
+                />
+                <Link
+                  href="/methods"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  <a>{t('common:nav-methods')}</a>
+                </Link>
+                {dropdown4 && <Dropdown4 />}
+              </li>
+              <li
+                className="nav-item small-font"
+                onMouseEnter={onMouseEnter2}
+                onMouseLeave={onMouseLeave2}
+              >
+                <img
+                  className="arrow-navbar"
+                  loading="lazy"
+                  src="/down-arrow-white.svg"
+                  height="13px"
+                  width="13px"
+                  alt="arrow down"
+                />
+                <Link
+                  href="/sponsor-home"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  <a>{t('common:nav-support')}</a>
+                </Link>
+                {dropdown2 && <Dropdown2 />}
+              </li>
+              <li
+                onMouseEnter={onMouseEnter5}
+                onMouseLeave={onMouseLeave5}
+                className="nav-item small-font"
+              >
+                <img
+                  className="arrow-navbar"
+                  loading="lazy"
+                  src="/down-arrow-white.svg"
+                  height="13px"
+                  width="13px"
+                  alt="arrow down"
+                />
+                <Link
+                  href="/contact"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  <a>{t('common:nav-info')}</a>
+                </Link>
+                {dropdown5 && <Dropdown5 />}
+              </li>
+              <li onClick={closeMobileMenu} className="nav-item small-font">
+                <a
+                  target="_blank"
+                  rel="noopener"
+                  href="https://cg-prospect.creator-spring.com"
+                  className="btn-line btn-merch"
+                >
+                  Merchandise
+                </a>
+              </li>
+              <li onClick={closeMobileMenu} className="nav-item small-font">
+                <a
+                  target="_blank"
+                  rel="noopener"
+                  href="https://www.patreon.com/cgprospect"
+                  className="nav-links patreon-clr btn btn2"
+                >
+                  Cahoot
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="menu-icon" onClick={handleClick}>
+            <span className="test" onClick={closeMobileMenu}>
+              X
+            </span>
+          </div>
+        </nav>
+      ) : (
+        <nav className="navbar">
+          <div className="navbar-logo-cnt">
+            <Link href="/">
+              <a>
+                <img
+                  src="/logo-navbar.svg"
+                  height="30px"
+                  width="160px"
+                  alt="main logo"
+                />
+              </a>
             </Link>
-            {dropdown3 && <Dropdown3 />}
-          </li>
-          <li
-            onMouseEnter={onMouseEnter4}
-            onMouseLeave={onMouseLeave4}
-            className="nav-item small-font"
-          >
-            <img
-              className="arrow-navbar"
-              loading="lazy"
-              src="/down-arrow-white.svg"
-              height="13px"
-              width="13px"
-              alt="arrow down"
-            />
-            <Link
-              href="/methods"
-              className="nav-links"
-              onClick={closeMobileMenu}
-            >
-              <a>{t('common:nav-methods')}</a>
-            </Link>
-            {dropdown4 && <Dropdown4 />}
-          </li>
-          <li
-            className="nav-item small-font"
-            onMouseEnter={onMouseEnter2}
-            onMouseLeave={onMouseLeave2}
-          >
-            <img
-              className="arrow-navbar"
-              loading="lazy"
-              src="/down-arrow-white.svg"
-              height="13px"
-              width="13px"
-              alt="arrow down"
-            />
-            <Link
-              href="/sponsor-home"
-              className="nav-links"
-              onClick={closeMobileMenu}
-            >
-              <a>{t('common:nav-support')}</a>
-            </Link>
-            {dropdown2 && <Dropdown2 />}
-          </li>
-          <li
-            onMouseEnter={onMouseEnter5}
-            onMouseLeave={onMouseLeave5}
-            className="nav-item small-font"
-          >
-            <img
-              className="arrow-navbar"
-              loading="lazy"
-              src="/down-arrow-white.svg"
-              height="13px"
-              width="13px"
-              alt="arrow down"
-            />
-            <Link
-              href="/contact"
-              className="nav-links"
-              onClick={closeMobileMenu}
-            >
-              <a>{t('common:nav-info')}</a>
-            </Link>
-            {dropdown5 && <Dropdown5 />}
-          </li>
-          <li onClick={closeMobileMenu} className="nav-item small-font">
-            <a
-              target="_blank"
-              rel="noopener"
-              href="https://cg-prospect.creator-spring.com"
-              className="btn-line btn-merch"
-            >
-              Merchandise
-            </a>
-          </li>
-          <li onClick={closeMobileMenu} className="nav-item small-font">
-            <a
-              target="_blank"
-              rel="noopener"
-              href="https://www.patreon.com/cgprospect"
-              className="nav-links patreon-clr btn btn2"
-            >
-              Cahoot
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className="menu-icon" onClick={handleClick}>
-        <span className="test" onClick={closeMobileMenu}>
-          X
-        </span>
-      </div>
-    </nav>
+          </div>
+          <LanguageChange />
+          <div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+              <li
+                className="nav-item small-font"
+                onMouseEnter={onMouseEnter}
+                onMouseLeave={onMouseLeave}
+              >
+                <img
+                  className="arrow-navbar"
+                  loading="lazy"
+                  src="/down-arrow-white.svg"
+                  height="13px"
+                  width="13px"
+                  alt="arrow down"
+                />
+                <Link
+                  href="/free-textures"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  <a>{t('common:nav-assets')}</a>
+                </Link>
+                {dropdown && <Dropdown />}
+              </li>
+              <li
+                onMouseEnter={onMouseEnter3}
+                onMouseLeave={onMouseLeave3}
+                className="nav-item small-font"
+              >
+                <img
+                  className="arrow-navbar"
+                  loading="lazy"
+                  src="/down-arrow-white.svg"
+                  height="13px"
+                  width="13px"
+                  alt="arrow down"
+                />
+                <Link
+                  href="/categories"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  <a>{t('common:nav-categories')}</a>
+                </Link>
+                {dropdown3 && <Dropdown3 />}
+              </li>
+              <li
+                onMouseEnter={onMouseEnter4}
+                onMouseLeave={onMouseLeave4}
+                className="nav-item small-font"
+              >
+                <img
+                  className="arrow-navbar"
+                  loading="lazy"
+                  src="/down-arrow-white.svg"
+                  height="13px"
+                  width="13px"
+                  alt="arrow down"
+                />
+                <Link
+                  href="/methods"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  <a>{t('common:nav-methods')}</a>
+                </Link>
+                {dropdown4 && <Dropdown4 />}
+              </li>
+              <li
+                className="nav-item small-font"
+                onMouseEnter={onMouseEnter2}
+                onMouseLeave={onMouseLeave2}
+              >
+                <img
+                  className="arrow-navbar"
+                  loading="lazy"
+                  src="/down-arrow-white.svg"
+                  height="13px"
+                  width="13px"
+                  alt="arrow down"
+                />
+                <Link
+                  href="/sponsor-home"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  <a>{t('common:nav-support')}</a>
+                </Link>
+                {dropdown2 && <Dropdown2 />}
+              </li>
+              <li
+                onMouseEnter={onMouseEnter5}
+                onMouseLeave={onMouseLeave5}
+                className="nav-item small-font"
+              >
+                <img
+                  className="arrow-navbar"
+                  loading="lazy"
+                  src="/down-arrow-white.svg"
+                  height="13px"
+                  width="13px"
+                  alt="arrow down"
+                />
+                <Link
+                  href="/contact"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  <a>{t('common:nav-info')}</a>
+                </Link>
+                {dropdown5 && <Dropdown5 />}
+              </li>
+              <li onClick={closeMobileMenu} className="nav-item small-font">
+                <a
+                  target="_blank"
+                  rel="noopener"
+                  href="https://cg-prospect.creator-spring.com"
+                  className="btn-line btn-merch"
+                >
+                  Merchandise
+                </a>
+              </li>
+              <li onClick={closeMobileMenu} className="nav-item small-font">
+                <a
+                  target="_blank"
+                  rel="noopener"
+                  href="https://www.patreon.com/cgprospect"
+                  className="nav-links patreon-clr btn btn2"
+                >
+                  Cahoot
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="menu-icon" onClick={handleClick}>
+            <span className="test" onClick={closeMobileMenu}>
+              X
+            </span>
+          </div>
+        </nav>
+      )}
+    </div>
   );
 }
 
