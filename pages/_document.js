98b516a0fc6ccfa6ace9be5046facb4ffae3c5de
IvanSmiths <info@ivansmiths.com>
2021-09-23 21:32:32 +0200

    gtm

--- pages/_document.js
+++ pages/_document.js
@@ -38,6 +38,16 @@ class MyDocument extends Document {
           `,
             }}
           />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5WQM4L8')`,
+            }}
+          />
           {/* Global Site Code Pixel - Facebook Pixel */}
           <script
             dangerouslySetInnerHTML={{
@@ -65,6 +75,14 @@ class MyDocument extends Document {
         </Head>
         <body>
           <Main>
+            <noscript>
+              <iframe
+                src="https://www.googletagmanager.com/ns.html?id=GTM-5WQM4L8"
+                height="0"
+                width="0"
+                style="display:none;visibility:hidden"
+              ></iframe>
+            </noscript>
             <script
               type="application/ld+json"
               dangerouslySetInnerHTML={{
