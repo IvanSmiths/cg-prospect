7483ed5232cb6b8354233f3edc158f24be1257e3
IvanSmiths <info@ivansmiths.com>
2021-11-19 13:07:56 +0100

    ADD 3d modsl

--- components/Homepage-stats.js
+++ components/Homepage-stats.js
@@ -68,7 +68,7 @@ function Stats() {
             {t('home:stats-num1-desc')}
           </li>
           <li className="numbers medium-font highlight font-color-inverse">
-            17 <br />
+            20 <br />
             {t('home:stats-num2-desc')}
           </li>
           {/* <li className="numbers">
