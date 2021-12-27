1c61b34610e57a6d5fd25ee93243edc8345914fb
IvanSmiths <info@ivansmiths.com>
2021-12-27 10:27:04 +0100

    ADD GTAG

--- pages/_document.js
+++ pages/_document.js
@@ -12,10 +12,10 @@ class MyDocument extends Document {
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
           ></script> */}
           {/* Global Site Tag (gtag.js) - Google Analytics */}
-          {/* <script
+          <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          /> */}
+          />
           <script
             dangerouslySetInnerHTML={{
               __html: `
@@ -29,27 +29,17 @@ class MyDocument extends Document {
           `,
             }}
           />
-
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-5WQM4L8')`,
-            }}
-          />
         </Head>
         <body>
           <Main>
-            <noscript>
+            {/* <noscript>
               <iframe
                 src="https://www.googletagmanager.com/ns.html?id=GTM-5WQM4L8"
                 height="0"
                 width="0"
                 style="display:none;visibility:hidden"
               ></iframe>
-            </noscript>
+            </noscript> */}
           </Main>
           <NextScript />
         </body>
